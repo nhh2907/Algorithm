ef35ce69b6e6178ad4d9bc75541349209e7f6ec3
Nogari <nhh2907@gmail.com>
2022-12-12 10:30:16 +0900

    next_number

---
+++ programmers/easy/next_number.py
@@ -0,0 +1,10 @@
+https://school.programmers.co.kr/learn/courses/30/lessons/120924
+def solution(common):
+    diff_1 = common[1] - common[0]
+    diff_2 = common[2] - common[1]
+    
+    if diff_1 - diff_2 == 0:
+        answer = common[-1] + diff_1
+    else: 
+        answer = (common[1] // common[0]) * common[-1]
+    return answer

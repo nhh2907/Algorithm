925359fb30d02e27af612e4e05087fbcc85f6c07
Nogari <nhh2907@gmail.com>
2022-12-14 11:31:45 +0900

    two sum

---
+++ Baekjoon/easy/dice.py
@@ -0,0 +1,25 @@
+a,b,c = map(int, input().split())
+def dice(a,b,c):
+    dic = {}
+    for i in range(1, a+1):
+        for j in range(1, b + 1):
+            for k in range(1, c + 1):
+                add = i + j + k
+                if add not in dic:
+                    dic[add] = 1
+                else:
+                    dic[add] += 1
+
+    # 첫번째 출력 방법
+    maximum = 0
+    for k, v in dic.items():
+        if maximum < v:
+            maximum = v
+            answer = k
+    print(answer)
+
+    # # 두번째 출력 방법
+    # answer2 = sorted(dic.items(), key=lambda x : x[1], reverse=True)
+    # print(answer2[0][0])
+
+dice(a,b,c)
---
+++ Baekjoon/easy/two_number.py
@@ -0,0 +1,8 @@
+a,b,c,d = input().split()
+def two_number(a,b,c,d):
+    P = int(a+b)
+    Q = int(c+d)
+
+    return P+Q
+
+print(two_number(a,b,c,d))

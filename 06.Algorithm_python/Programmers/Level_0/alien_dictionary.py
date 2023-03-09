4cfd41f16eb27d5dc553f77b28d3c06a4a5963df
Nogari <nhh2907@gmail.com>
2023-03-09 18:01:25 +0900

    added code

--- 06.Algorithm_python/Programmers/Level_0/alien_dictionary.py
+++ 06.Algorithm_python/Programmers/Level_0/alien_dictionary.py
@@ -13,3 +13,10 @@ def solution(spell, dic):
         if not set(spell) - set(word):
             return 1
     return 2
+
+# best
+def solution(spell, dic):
+    for word in dic:
+        if sorted(spell) == sorted(word):
+            return 1
+    return 2  

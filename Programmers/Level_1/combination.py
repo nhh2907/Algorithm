6457c02d34ddfd284a452636b08938943736ffab
HYUNHO NOH <nhh2907@gmail.com>
2023-03-25 00:23:10 +0900

    Update combination.py

--- Programmers/Level_1/combination.py
+++ Programmers/Level_1/combination.py
@@ -7,13 +7,14 @@ def solution(numbers):
     return sorted(set(answer))
 
 
-# 조합 만들기 방법 11
+# 조합 만들기 방법 1
 def solution(numbers):
 	idx = [(i,j) for i in range(len(numbers)) for j in range(len(numbers)) if i<j]
 	answer = set([numbers[i]+numbers[j] for i,j in idxs])
 	return = answer
 
 
+# 조합 만들기 방법 2
 from itertools import combinations
 def solution(numbers):
     answer = []

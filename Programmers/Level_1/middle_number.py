7dfa5e994f418a92281af10f004e5cdad006769c
Nogari <78652856+nhh2907@users.noreply.github.com>
2023-04-07 20:29:51 +0900

    83%

---
+++ Programmers/Level_1/middle_number.py
@@ -0,0 +1,10 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12903
+def solution(s):
+    l = len(s)
+    if l%2 == 0:
+        return s[l//2-1:l//2+1]
+    else:
+        return s[l//2]
+    
+def solution(s):
+    return s[(len(s)-1)//2 : len(s)//2 + 1]
